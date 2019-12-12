f8361652deb399daf33ab59a91ed5cef676db63b
Ryan Li <1078054388@qq.com>
2019-12-12 15:30:52 +0800

    使用mybatis-generator生成POJO、mapper和xml文件

--- src/main/java/com/ryan/mall/pojo/Category.java
+++ src/main/java/com/ryan/mall/pojo/Category.java
@@ -10,12 +10,17 @@ import java.util.Date;
  */
 @Data
 public class Category {
-
     private Integer id;
+
     private Integer parentId;
+
     private String name;
+
     private Integer status;
+
     private Integer sortOrder;
+
     private Date createTime;
+
     private Date updateTime;
 }
